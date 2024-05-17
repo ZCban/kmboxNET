e3a8f2dd12824a0e26a6a15bebe487b9db005cda
ZCban <100644497+ZCban@users.noreply.github.com>
2024-05-17 21:18:07 +0200

    Update calldemo.cpp

--- c+++demo/calldemo.cpp
+++ c+++demo/calldemo.cpp
@@ -1,4 +1,4 @@
-﻿// kmNetLib.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// kmNetLib.cpp : This file contains the "main" function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -6,216 +6,187 @@
 #include "NetConfig/HidTable.h"
 #include "picture.h"
 
+int main() {
+    int ret;
 
+    // Initial connection test - Must connect successfully before using other APIs
+    printf("---------------- This program is a demo for kmboxNet version call test --------------\r\n");
+    ret = kmNet_init((char*)"192.168.2.188", (char*)"8338", (char*)"24ba5054"); // Connect to the box
+    // If this function does not return, it means IP or UUID is incorrect. Please check if it matches the display on the box screen
 
-int main()
-{
-	//连接测试  必须连接正常才能操作其他API 
-	 int ret;
-	 printf("----------------本程序为kmboxNet版本调用测试demo--------------\r\n");
-	 ret= kmNet_init((char*)"192.168.2.188", (char*)"8338", (char*)"24ba5054"); //连接盒子
-	 //如果此函数不返回，则说明IP，UUID错误。请检查是否和盒子显示屏上显示一致
+    kmNet_monitor(10000); // Use port 10000 to monitor physical keyboard and mouse messages, ensure the port does not conflict with other programs
+    Sleep(1000);
 
+    while (true) {
+        if (kmNet_monitor_mouse_left() == 1) { // If the left mouse button is pressed
+            printf("Left mouse button pressed\r\n");
+            while (kmNet_monitor_mouse_left() == 1) Sleep(1); // Wait for the left button to be released
+            kmNet_monitor(0); // Disable monitoring
+        }
+        Sleep(1); // Sleep to avoid high CPU usage
+    }
 
-	 kmNet_monitor(10000);//用10000端口来监听物理键鼠消息 ，端口号不要与其他程序冲突即可
-	 Sleep(1000);
-	 for (;;)
-	 {	
-		 if (kmNet_monitor_mouse_left()==1) //如果鼠标左键按下
-		 {
-			 printf("鼠标左键按下\r\n");
-			 while (kmNet_monitor_mouse_left()==1) Sleep(1);//等待左键松开
-			 kmNet_monitor(0);	//关闭监控功能
-		 }
-		 Sleep(1);//主动休眠避免CPU占用过高
-	 }
+    printf("Call speed test, please wait about 10 seconds...\r\n");
+    int cnt = 10000;
+    long startime = GetTickCount();
+    while (cnt > 0) {
+        ret = kmNet_mouse_move(0, -100); cnt--;
+        if (ret)
+            printf("tx error %d  ret0=%d\r\n", cnt, ret);
+        ret = kmNet_mouse_move(0, 100);  cnt--;
+        if (ret)
+            printf("tx error %d  ret1=%d\r\n", cnt, ret);
+    }
+    printf("\t10000 calls took = %ld ms\r\n", GetTickCount() - startime);
 
+    printf("Display test:\r\n");
+    while (1) {
+        startime = GetTickCount();
+        kmNet_lcd_color(0);
+        printf("\tFull screen solid color fill took = %ld ms\r\n", GetTickCount() - startime);
+        startime = GetTickCount();
+        kmNet_lcd_picture_bottom(gImage_128x80); // Display 128x80 image on the bottom half
+        printf("\tHalf screen picture refresh took = %ld ms\r\n", GetTickCount() - startime);
+        startime = GetTickCount();
+        kmNet_lcd_picture(gImage_128x160); // Display 128x160 image on the full screen
+        printf("\tFull screen picture refresh took = %ld ms\r\n", GetTickCount() - startime);
+        break;
+    }
 
-
-	 printf("调用速度测试,请等待约10s左右...\r\n");
-	 int cnt = 10000;
-	 long startime = GetTickCount();
-	 while (cnt > 0)
-	 {
-		 ret = kmNet_mouse_move(0, -100); cnt--;
-		 if (ret)
-			 printf("tx error %d  ret0=%d\r\n", cnt, ret);
-		 ret = kmNet_mouse_move(0, 100);  cnt--;
-		 if (ret)
-			 printf("tx error %d  ret1=%d\r\n", cnt, ret);
-	 }
-	 printf("\t10000次调用耗时=%ld ms\r\n", GetTickCount() - startime);
-
-
-	 printf("显示屏测试：\r\n");
-	 while(1)
-	 {	 startime = GetTickCount();
-		 kmNet_lcd_color(0);
-		 printf("\t整屏纯色填充耗时 =%ld ms\r\n", GetTickCount() - startime);
-		 startime = GetTickCount();
-		 kmNet_lcd_picture_bottom(gImage_128x80); //下半部分显示128x80图片
-		 printf("\t半屏图片刷新耗时=%ld ms\r\n", GetTickCount() - startime);
-		 startime = GetTickCount();
-		 kmNet_lcd_picture(gImage_128x160);		  //整屏显示128x160图片
-		 printf("\t整屏图片刷新耗时 =%ld ms\r\n", GetTickCount() - startime);
-		 break;
-	 }
 #if 0
-	kmNet_init((char*)"192.168.2.166", (char*)"1234", (char*)"F101383B"); //连接盒子
-	printf("修改盒子IP地址\r\n");
-	kmNet_setconfig((char*)"192.168.2.166", 1234);
-	printf("修改后需要重启才能生效\r\n");
-	kmNet_reboot();
+    kmNet_init((char*)"192.168.2.166", (char*)"1234", (char*)"F101383B"); // Connect to the box
+    printf("Change box IP address\r\n");
+    kmNet_setconfig((char*)"192.168.2.166", 1234);
+    printf("Reboot is required to take effect\r\n");
+    kmNet_reboot();
 #endif
 
+    // Physical keyboard and mouse monitoring function test
+    printf("Physical keyboard and mouse monitoring function, press left, middle, and right buttons to exit the test\r\n");
+    printf("Note: If you cannot monitor the key information, please turn off the Windows firewall\r\n");
+    kmNet_monitor(1000); // Enable keyboard and mouse monitoring function, listening port is 1000
+    int exit = 0;
+    while ((exit & 0x07) != 0x07) {
+        // Mouse button detection
+        if (kmNet_monitor_mouse_left() == 1) { // Left mouse button pressed
+            printf("Left mouse button pressed\r\n");
+            do { Sleep(1); } while (kmNet_monitor_mouse_left() == 1); // Wait for the left button to be released
+            printf("Left mouse button released\r\n");
+            exit |= 0x01;
+        }
 
-	//监听物理键鼠功能测试
-	printf("监听物理键鼠功能,按完左中右键后就能退出测试\r\n");
-	printf("注意。如果无法监听到按键信息，请关闭windows防火墙\r\n");
-	kmNet_monitor(1000); //开启键鼠监控功能 监听端口为1000 
-	int exit = 0;
-	while ((exit&0x07)!=0x07)
-	{
-		//鼠标按键检测
-		if (kmNet_monitor_mouse_left() == 1) //鼠标左键按下了
-		{
-			printf("鼠标左键按下\r\n");
-			do { Sleep(1); } while (kmNet_monitor_mouse_left() == 1); //等待左键松开
-			printf("鼠标左键松开\r\n");
-			exit |= 0x01;
-		}
+        if (kmNet_monitor_mouse_middle() == 1) { // Middle mouse button pressed
+            printf("Middle mouse button pressed\r\n");
+            do { Sleep(1); } while (kmNet_monitor_mouse_middle() == 1); // Wait for the middle button to be released
+            printf("Middle mouse button released\r\n");
+            exit |= 0x02;
+        }
 
-		if (kmNet_monitor_mouse_middle() == 1) //鼠标中键按下了
-		{
-			printf("鼠标中键按下\r\n");
-			do { Sleep(1); } while (kmNet_monitor_mouse_middle() == 1); //等待左键松开
-			printf("鼠标中键松开\r\n");
-			exit |= 0x02;
-		}
+        if (kmNet_monitor_mouse_right() == 1) { // Right mouse button pressed
+            printf("Right mouse button pressed\r\n");
+            do { Sleep(1); } while (kmNet_monitor_mouse_right() == 1); // Wait for the right button to be released
+            printf("Right mouse button released\r\n");
+            exit |= 0x04;
+        }
 
-		if (kmNet_monitor_mouse_right() == 1) //鼠标中键按下了
-		{
-			printf("鼠标右键按下\r\n");
-			do { Sleep(1); } while (kmNet_monitor_mouse_right() == 1); //等待左键松开
-			printf("鼠标右键松开\r\n");
-			exit |= 0x04;
-		}
+        if (kmNet_monitor_mouse_side1() == 1) { // Side mouse button 1 pressed
+            printf("Side mouse button 1 pressed\r\n");
+            do { Sleep(1); } while (kmNet_monitor_mouse_side1() == 1); // Wait for side button 1 to be released
+            printf("Side mouse button 1 released\r\n");
+            exit |= 0x08;
+        }
 
-		if (kmNet_monitor_mouse_side1() == 1) //鼠标中键按下了
-		{
-			printf("鼠标侧键1按下\r\n");
-			do { Sleep(1); } while (kmNet_monitor_mouse_side1() == 1); //等待左键松开
-			printf("鼠标侧键1松开\r\n");
-			exit |= 0x08;
-		}
+        if (kmNet_monitor_mouse_side2() == 1) { // Side mouse button 2 pressed
+            printf("Side mouse button 2 pressed\r\n");
+            do { Sleep(1); } while (kmNet_monitor_mouse_side2() == 1); // Wait for side button 2 to be released
+            printf("Side mouse button 2 released\r\n");
+            exit |= 0x10;
+        }
 
-		if (kmNet_monitor_mouse_side2() == 1) //鼠标中键按下了
-		{
-			printf("鼠标侧键2按下\r\n");
-			do { Sleep(1); } while (kmNet_monitor_mouse_side2() == 1); //等待左键松开
-			printf("鼠标侧键2松开\r\n");
-			exit |= 0x10;
-		}
+        if (kmNet_monitor_keyboard(KEY_A) == 1) { // Keyboard A key pressed
+            printf("Keyboard A key pressed\r\n");
+            do { Sleep(1); } while (kmNet_monitor_keyboard(KEY_A) == 1); // Wait for the A key to be released
+            printf("Keyboard A key released\r\n");
+            exit |= 0x20;
+        }
+        Sleep(1);
+    }
 
-		if (kmNet_monitor_keyboard(KEY_A) == 1) //鼠标中键按下了
-		{
-			printf("键盘A键按下\r\n");
-			do { Sleep(1); } while (kmNet_monitor_keyboard(KEY_A) == 1); //等待左键松开
-			printf("键盘A键松开\r\n");
-			exit |= 0x20;
-		}
-		Sleep(1);
-	}
+    // Simulate artificial trajectory test
+    while (true) {
+        // Open the drawing tool and press the left mouse button to see the movement trajectory
+        if (kmNet_monitor_mouse_left()) { // If the left mouse button is pressed
+            startime = GetTickCount();
+            ret = kmNet_mouse_move_auto(400, 300, 200); // Expected to complete within 200ms
+            printf("\tTime taken = %ld ms ret = %d\r\n", GetTickCount() - startime, ret); // Actual time taken
+            while (kmNet_monitor_mouse_left()) Sleep(1); // Wait for the left button to be released
+        }
+        Sleep(1); // Sleep to avoid high CPU usage
+    }
 
+#if 0 // Mask mouse test
+    printf("Physical keyboard and mouse masking test:\r\n");
+    kmNet_monitor(1); // Enable keyboard and mouse monitoring function
+    kmNet_mask_mouse_left(1); // Mask left mouse button
+    kmNet_mask_mouse_middle(1); // Mask middle mouse button
+    kmNet_mask_mouse_right(1); // Mask right mouse button
+    kmNet_mask_mouse_side1(1); // Mask side mouse button 1
+    kmNet_mask_mouse_side2(1); // Mask side mouse button 2
+    kmNet_mask_mouse_x(1); // Mask mouse x direction
+    kmNet_mask_mouse_y(1); // Mask mouse y direction -- with the above code enabled, the mouse is basically useless
+    kmNet_mask_keyboard(KEY_A); // Mask keyboard A key
+    int timeout = 1000;
+    while (true) { // Exit after 10 seconds
+        if (kmNet_monitor_mouse_left()) { // If the physical left mouse button is pressed
+            printf("Physical left mouse button pressed\r\n");
+            while (kmNet_monitor_mouse_left()) Sleep(1); // Wait for the left button to be released
+        }
+        if (kmNet_monitor_mouse_middle()) { // If the physical middle mouse button is pressed
+            printf("Physical middle mouse button pressed\r\n");
+            while (kmNet_monitor_mouse_middle()) Sleep(1); // Wait for the middle button to be released
+        }
+        if (kmNet_monitor_mouse_right()) { // If the physical right mouse button is pressed
+            printf("Physical right mouse button pressed\r\n");
+            while (kmNet_monitor_mouse_right()) Sleep(1); // Wait for the right button to be released
+        }
+        if (kmNet_monitor_mouse_side1()) { // If the physical side mouse button 1 is pressed
+            printf("Physical side mouse button 1 pressed\r\n");
+            while (kmNet_monitor_mouse_side1()) Sleep(1); // Wait for side button 1 to be released
+        }
 
+        if (kmNet_monitor_mouse_side2()) { // If the physical side mouse button 2 is pressed
+            printf("Physical side mouse button 2 pressed\r\n");
+            while (kmNet_monitor_mouse_side2()) Sleep(1); // Wait for side button 2 to be released
+        }
 
-
- //模拟人工轨迹测试
-	for (;;)
-	{	/*打开画图工具按下鼠标左键看移动轨迹*/
-		if (kmNet_monitor_mouse_left()) //如果鼠标左键按下
-		{
-			 startime = GetTickCount();
-			 ret = kmNet_mouse_move_auto(400, 300, 200);						//预设200ms内完成
-			printf("\t耗时=%ld ms ret=%d\r\n", GetTickCount() - startime, ret);	//实际耗时
-			while (kmNet_monitor_mouse_left()) Sleep(1);						//等待左键松开
-		}
-		Sleep(1);//主动休眠避免CPU占用过高
-	}
-	
-#if 0 //屏蔽鼠标测试
-	printf("物理键鼠屏蔽测试：\r\n");
-	kmNet_monitor(1);			//开启键鼠监控功能
-	kmNet_mask_mouse_left(1);	//屏蔽鼠标左键
-	kmNet_mask_mouse_middle(1);	//屏蔽鼠标中键
-	kmNet_mask_mouse_right(1);	//屏蔽鼠标右键
-	kmNet_mask_mouse_side1(1);	//屏蔽鼠标侧键1
-	kmNet_mask_mouse_side2(1);	//屏蔽鼠标侧键2
-	kmNet_mask_mouse_x(1);		//屏蔽鼠标x方向
-	kmNet_mask_mouse_y(1);		//屏蔽鼠标y方向  --开启以上代码后鼠标基本上就没啥用了
-	kmNet_mask_keyboard(KEY_A);	//屏蔽键盘A键
-	int timeout = 1000;
-	for (;;)// 10秒后退出
-	{
-		if (kmNet_monitor_mouse_left()) {				//如果物理鼠标左键按下
-			printf("物理鼠标左键按下\r\n");
-			while (kmNet_monitor_mouse_left()) Sleep(1);//等待左键松开
-		}
-		if (kmNet_monitor_mouse_middle()) {				  //如果物理鼠标中键按下
-			printf("物理鼠标中键按下\r\n");
-			while (kmNet_monitor_mouse_middle()) Sleep(1);//等待中键松开
-		}
-		if (kmNet_monitor_mouse_right()) {				  //如果物理鼠标右键按下
-			printf("物理鼠标右键按下\r\n");
-			while (kmNet_monitor_mouse_right()) Sleep(1);//等待右键松开
-		}
-		if (kmNet_monitor_mouse_side1()) {				  //如果物理鼠标侧键1按下
-			printf("物理鼠标右键按下\r\n");
-			while (kmNet_monitor_mouse_side1()) Sleep(1);//等待侧键1松开
-		}
-
-		if (kmNet_monitor_mouse_side2()) {				  //如果物理鼠标侧键2按下
-			printf("物理鼠标右键按下\r\n");
-			while (kmNet_monitor_mouse_side2()) Sleep(1);//等待侧键2松开
-		}
-
-		if (kmNet_monitor_keyboard(KEY_A))
-		{
-			printf("键盘A键按下\r\n");
-			while (kmNet_monitor_keyboard(KEY_A)) Sleep(1);//等待侧键2松开
-			printf("键盘A键松开\r\n");
-		}
-		Sleep(1);//主动休眠避免CPU占用过高
-		if (timeout == 0) break;
-		timeout--;
-
-	}
-	printf("解除所有屏蔽\r\n");
-	kmNet_unmask_all();//解除所有屏蔽。此时键盘鼠标都能正常使用
-#endif 
-
+        if (kmNet_monitor_keyboard(KEY_A)) {
+            printf("Keyboard A key pressed\r\n");
+            while (kmNet_monitor_keyboard(KEY_A)) Sleep(1); // Wait for the A key to be released
+            printf("Keyboard A key released\r\n");
+        }
+        Sleep(1); // Sleep to avoid high CPU usage
+        if (timeout == 0) break;
+        timeout--;
+    }
+    printf("Unmask all\r\n");
+    kmNet_unmask_all(); // Unmask all. Now the keyboard and mouse can be used normally
+#endif
 
 #if 0
-	 kmNet_mouse_left(0);			//松开
-	 kmNet_mouse_all(1,0,0,0);		//按下
-	 kmNet_mouse_all(0, 0, 0, 0);	//松开
-	 kmNet_mouse_right(1);			//按下
-	//键盘按键测试
-	 kmNet_keydown(4);//a按下
-	 kmNet_reboot();  //重启盒子
-	 kmNet_mouse_right(0);			//松开
-	 kmNet_mouse_all(2, 0, 0, 0);	//按下
-	 kmNet_mouse_all(0, 0, 0, 0);	//松开
-	 //鼠标中键测试
-	 kmNet_mouse_middle(1);			//按下
-	 kmNet_mouse_middle(0);			//松开
-	 kmNet_mouse_all(4, 0, 0, 0);	//按下
-	 kmNet_mouse_all(0, 0, 0, 0);	//松开
-
-#endif 
-
-
-
-
-
+    kmNet_mouse_left(0); // Release
+    kmNet_mouse_all(1, 0, 0, 0); // Press
+    kmNet_mouse_all(0, 0, 0, 0); // Release
+    kmNet_mouse_right(1); // Press
+    // Keyboard key test
+    kmNet_keydown(4); // a key press
+    kmNet_reboot(); // Reboot the box
+    kmNet_mouse_right(0); // Release
+    kmNet_mouse_all(2, 0, 0, 0); // Press
+    kmNet_mouse_all(0, 0, 0, 0); // Release
+    // Middle mouse button test
+    kmNet_mouse_middle(1); // Press
+    kmNet_mouse_middle(0); // Release
+    kmNet_mouse_all(4, 0, 0, 0); // Press
+    kmNet_mouse_all(0, 0, 0, 0); // Release
+#endif
 }
